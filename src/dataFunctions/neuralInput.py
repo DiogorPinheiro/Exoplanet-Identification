146403043bc16cd8ef84028b118367548037054d
DiogorPinheiro <diogomrpinheiro@gmail.com>
2020-04-08 09:52:49 +0100

    earth++

--- src/dataFunctions/neuralInput.py
+++ src/dataFunctions/neuralInput.py
@@ -1,5 +1,4 @@
 import numpy as np
-import pickle
 from sklearn import preprocessing
 import lightkurve as lk
 import pandas as pd
@@ -7,18 +6,15 @@ import csv
 import time as t
 import matplotlib.pyplot as plt
 
-# from dataFunctions import dataCleaner
-# from dataFunctions import dataInfo
-# from dataFunctions import dataAnalyzer
-# from dataFunctions import dataReader
-
 import dataInfo as di
 import dataCleaner as dc
-# import dataReader as dr
 
-CSV_FILE = "/home/jcneves/Documents/Identifying-Exoplanets-Using-ML/src/q1_q17_dr24_tce_2020.01.28_08.52.13.csv"
-#csvmac = "/Users/diogopinheiro/Documents/Engenharia Informática/3º Ano/2º Semestre/Projeto/Identifying-Exoplanets-Using-ML/src/q1_q17_dr24_tce_2020.01.28_08.52.13.csv"
+# Directories
+#CSV_FILE = "/home/jcneves/Documents/Identifying-Exoplanets-Using-ML/src/q1_q17_dr24_tce_2020.01.28_08.52.13.csv"
+csvmac = "/Users/diogopinheiro/Documents/Engenharia Informática/3º Ano/2º Semestre/Projeto/Identifying-Exoplanets-Using-ML/src/q1_q17_dr24_tce_2020.01.28_08.52.13.csv"
 DATA_DIRECTORY = "/home/jcneves/Documents/keplerData"
+
+# File Names
 GLOBAL_CSV = "../neural_input_global.csv"
 LOCAL_CSV = "../neural_input_local.csv"
 GLOBAL_CSV_SOVGOL = "../neural_input_global_sovgol.csv"
@@ -35,27 +31,37 @@ def getCSVData():
 
         Output: Pandas Dataframe
     '''
-    return di.dataCSV(CSV_FILE)
+    return di.dataCSV(csvmac)
 
 
 def globaView(curve):
+    '''
+        Obtain Global View Of The Light Curve
+
+        Input: curve (LightCurve Object)     
+        Output: lc_global (LightCurve Object)  
+    '''
     # Fixed Length 2001 Bins
     lc_global = curve.bin(bins=2001, method='median').normalize() - 1
     lc_global = (lc_global / np.abs(lc_global.flux.min())) * 2.0 + 1
     # print(lc_global.flux.shape)
-    # lc_global.scatter()
     return lc_global
 
 
 def localView(curve, f_dur):
+    '''
+        Obtain Local View Of The Light Curve
+
+        Input: curve (LightCurve Object) and f_dur (fractional duration -> float)    
+        Output: lc_local (LightCurve Object) or False     
+    '''
     phase_mask = (curve.phase > -4 * f_dur) & (curve.phase < 4.0 * f_dur)
     lc_zoom = curve[phase_mask]
-    if lc_zoom:
+    if lc_zoom:  # Avoid Empty Signals
         # Fixed Length 201 Bins
         lc_local = lc_zoom.bin(bins=201, method='median').normalize() - 1
         lc_local = (lc_local / np.abs(lc_local.flux.min())) * 2.0 + 1
         # print(lc_local.flux.shape)
-        # lc_global.scatter()
         return lc_local
     else:
         return False
@@ -79,66 +85,87 @@ def getLabel(table, kepid, index):
 
 
 def writeFile(file, row):
+    '''
+        Append Row Of Values To File
+
+        Input: file (String) and row (Numpy Array) 
+    '''
     with open(file, 'a') as fd:
         writer = csv.writer(fd)
         writer.writerow(row)
 
 
+def createImage(name, phase, flux):
+    '''
+        Plot Light Curve Views And Save To Folder
+
+        Input: name (String), phase (Numpy Array) and flux (Numpy Array)
+    '''
+    fig, ax = plt.subplots(nrows=1, ncols=1)  # create figure & 1 axis
+    ax.plot(phase, flux)
+    fig.savefig(name)   # save the figure to file
+    plt.close(fig)    # close the figure window
+
+
 def main():
+    '''
+        1. Read TCE Table And Extract Kepids
+        2. Obtain Light Curve For Each Kepid
+        3. Pre-Process That Light Curve (Folding + Binning + Optional Filter )
+        4. Generate Local And Global View
+        5. Write Views To File
+
+        Output: 2 CSV Files (Global View And Local View)
+
+    '''
     start = t.time()
-    table = getCSVData().drop_duplicates()
+    table = getCSVData()
+    print(len(table))
     kepids = di.listKepids(table)  # List of Kepids
     # dataReader.createFluxDatabase(table,kepids,DATA_DIRECTORY)
 
-    file = open("global_movavg_new.csv", "w")
-    file2 = open("local_movavg_new.csv", "w")
+    # Open Files For Global And Local View
+    file = open(GLOBAL_CSV_MOVAVG, "w")
+    file2 = open(LOCAL_CSV_MOVAVG, "w")
     file.close()
     file2.close()
 
     global_flux = []
     local_flux = []
-    lc_raw_keeper = []
-    #kepids = [1162345, 1162345]
-    kepids = kepids[:10]
+
     count = 0
     for i, kep in enumerate(kepids):
-        # filenames = dr.filenameWarehouse(kep, dir_mac)  # Get Full Path Of All Light Curves
-        # Read Light Curves And Obtain Time And Flux
-        # time, flux = dr.fitsConverter(filenames)
-        # out_flux, out_time = dr.getConcatenatedLightCurve(flux, time)  # Concatenate All Light Curves
-        # out_flux = dc.sovitzGolay(out_flux)
-        count += 1
-        name = 'KIC {}'.format(kep)
+
+        name = 'KIC {}'.format(kep)  # Create Name (Ex: KIC 1162345)
         print(name)
         print("{}/{}".format(count, len(kepids)))
 
-        lcfs = lk.search_lightcurvefile(
-            name, mission='kepler', cadence='long', quarter=[1, 2, 3, 4, 5, 6, 7])
-        if lcfs:    # Avoid empty objects
-            lcfs = lcfs[:7].download_all()
+        label = getLabel(table, kep, i)
+        if label != 2:  # Avoid Light Curves With Label 'UNK'
+            lcfs = lk.search_lightcurvefile(
+                name, mission='kepler', cadence='long', quarter=[1, 2, 3, 4, 5, 6, 7])
+            if lcfs:    # Avoid empty objects
+                lcfs = lcfs[:7].download_all()
 
-            try:    # Avoid file with more than 1 target
-                # Combine all PDCSAP_FLUX extensions into a single lightcurve object
-                lc_raw = lcfs.PDCSAP_FLUX.stitch()
-                lc_raw_keeper.append(lc_raw)
-                # Clean outliers that are above mean level
-                lc_clean = lc_raw.remove_outliers(sigma=20, sigma_upper=4)
-            except:
+                try:    # Avoid file with more than 1 target
+                    # Combine all PDCSAP_FLUX extensions into a single lightcurve object
+                    lc_raw = lcfs.PDCSAP_FLUX.stitch()
+                    # Clean outliers that are above mean level
+                    lc_clean = lc_raw.remove_outliers(sigma=20, sigma_upper=4)
+                except:
+                    continue
+
+                # Get Info About Host Star
+                period = di.getTCEPeriod(table, kep, i)    # Period
+                duration = di.getTCEDuration(
+                    table, kep, i)    # Transit Duration
+                tO = di.getTCETransitMidpoint(table, kep, i)   # Transit Epoch
+                label = getLabel(table, kep, i)
+
+            else:   # If lcfs == 'NoneType'
                 continue
 
-            # Get Info About Host Star
-            period = di.getTCEPeriod(table, kep, i)    # Period
-            duration = di.getTCEDuration(table, kep, i)    # Transit Duration
-            tO = di.getTCETransitMidpoint(table, kep, i)   # Transit Epoch
-            label = getLabel(table, kep, i)
-
-        else:   # If lcfs == 'NoneType'
-            continue
-
-        if label != 2:
             # Signal Pre-processing
-            # lc = lk.LightCurve(out_time, out_flux)0
-            # lc_clean = lc.remove_outliers( sigma=20, sigma_upper=4)    # Clean Outliers
             temp_fold = lc_clean.fold(period, t0=tO)
             fractional_duration = (duration / 24.0) / period
             phase_mask = np.abs(temp_fold.phase) < (fractional_duration * 1.5)
@@ -154,7 +181,8 @@ def main():
             global_view = globaView(lc_fold)  # Global View
             gl_phase = global_view.phase
             gl_bri = global_view.flux
-            gl_bri = dc.movingAverage(gl_bri, 15)
+            gl_bri = dc.movingAverage(gl_bri, 15)  # Apply Filter
+            # Replace Nan Values With Mean
             np.nan_to_num(gl_bri, nan=np.mean(gl_bri))
             global_info = []
             global_info.append(kep)
@@ -167,8 +195,9 @@ def main():
             else:
                 lc_phase = local_view.phase
                 lc_bri = local_view.flux
-                lc_bri = dc.movingAverage(lc_bri, 15)
+                lc_bri = dc.movingAverage(lc_bri, 15)   # Apply Filter
 
+                # Replace Nan Values With Mean
                 np.nan_to_num(lc_bri, nan=np.mean(lc_bri))
                 local_info = []
                 local_info.append(kep)
@@ -177,34 +206,19 @@ def main():
 
         # Avoid Time Series With Just Nan Values and Force Correspondance Between Views
             if not (np.isnan(lc_bri).any() or np.isnan(gl_bri).any()):
-                # fig, ax = plt.subplots(nrows=1, ncols=1)  # create figure & 1 axis
-                #ax.plot(lc_phase, lc_bri)
-                #name = '../Images/Views/Local_' + str(kep) + ".png"
-                # fig.savefig(name)   # save the figure to file
-                # plt.close(fig)    # close the figure window
+                # Save Global And Local Views Plots
+                #name_1ocal = '../Images/Views/Local_' + str(kep) + ".png"
+                #createImage(name_local, lc_phase, lc_bri)
+                #name_global = '../Images/Views/Global_' + str(kep) + ".png"
+                #createImage(name_global, gl_phase, gl_bri)
 
-                # fig, ax = plt.subplots(nrows=1, ncols=1)  # create figure & 1 axis
-                #ax.plot(gl_phase, gl_bri)
-                #name = '../Images/Views/Global_' + str(kep) + ".png"
-                # fig.savefig(name)   # save the figure to file
-                # plt.close(fig)    # close the figure window
-
+                # Write Local And Global View Flux Values To File
                 local_info.append(label)
-                writeFile("local_movavg_new.csv", local_info)
+                writeFile(LOCAL_CSV_MOVAVG, local_info)
                 global_info.append(label)
-                writeFile("global_movavg_new.csv", global_info)
+                writeFile(GLOBAL_CSV_MOVAVG, global_info)
 
-    # with open(GLOBAL_CSV, 'w') as fd:
-    #    for row in global_flux:
-    #        writer = csv.writer(fd)
-    #        writer.writerow(row)
-
-    # with open(LOCAL_CSV, 'w') as fd:
-    #    for row in local_flux:
-    #        writer = csv.writer(fd)
-    #        writer.writerow(row)
-
-    # pickle.dump(lc_raw_keeper, open("lc_raw.p", "wb"))
+        count += 1
 
     end = t.time()
     print(end - start)
