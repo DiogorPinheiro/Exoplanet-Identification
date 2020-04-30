98d1ee1deab13dc78d57b50b2ccb9d8f7486d76f
DiogorPinheiro <diogomrpinheiro@gmail.com>
2020-04-30 18:06:06 +0100

    earth++

--- src/tuner.py
+++ src/tuner.py
@@ -1,9 +1,9 @@
 import numpy as np
 import tensorflow as tf
-import keras
+from tensorflow import keras
 from kerastuner import Hyperband
 from kerastuner.engine.hyperparameters import HyperParameters
-import shuffle
+#import shuffle
 from keras import optimizers
 from sklearn.model_selection import train_test_split
 from keras.layers import Dense, Input, concatenate, Flatten, Dropout, PReLU, BatchNormalization, Activation, GaussianNoise, MaxPooling1D
@@ -12,12 +12,12 @@ from keras.layers.convolutional import Conv1D
 from training import f1_m, precision_m, recall_m, mainEvaluate, auc_roc
 
 data_global = np.loadtxt(
-    'train_global.csv', delimiter=',')
+    'global_train.csv', delimiter=',')
 global_X = data_global[0:, 0:-1]  # Input
 global_Y = data_global[0:, -1]  # Labels
 
 # Suffle Data (Only For Shallue Datasets)
-global_X, global_Y = shuffle(global_X, global_Y)
+#global_X, global_Y = shuffle(global_X, global_Y)
 X_train_global, X_test_global, y_train_global, y_test_global = train_test_split(
     global_X, global_Y, test_size=0.2, random_state=1)
 
@@ -49,12 +49,18 @@ def build_model(hp):
     outputs = tf.keras.layers.Dense(1, activation='sigmoid')(x)
 
     model = tf.keras.Model(inputs, outputs)
-    model.compile(optimizer=keras.optimizers.Adam(hp.Choice('learning_rate', values=[1e-2, 1e-3])),
+    model.compile(optimizer=tf.keras.optimizers.Adam(hp.Choice('learning_rate', values=[1e-2, 1e-3])),
                   loss='binary_crossentropy',
-                  metrics=['accuracy', f1_m, precision_m, recall_m, auc_roc])
+                  metrics=['accuracy', f1_m, precision_m, recall_m,tf.keras.metrics.AUC()])
     return model
 
 
 tuner = Hyperband(build_model, objective='val_loss', max_epochs=40,)
 tuner.search(X_train_global_shaped, y_train_global,
              epochs=40, validation_data=(X_test_global_shaped, y_test_global))
+best_model = tuner.get_best_models(1)[0]
+best_model.summary()
+best_param = tuner.get_best_hyperparameters(1)[0]
+print(best_param.values)
+
+best_model.save('model.hdf5')
