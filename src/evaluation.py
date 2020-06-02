1d6832bd88d1fc263590c41e87c23337c66fc2bd
DiogorPinheiro <diogomrpinheiro@gmail.com>
2020-06-02 11:41:11 +0100

    earth++

--- src/evaluation.py
+++ src/evaluation.py
@@ -66,6 +66,58 @@ def evaluate(model_name, data_X, data_y):
     print("---------------------------------------------------")
 
 
+def evaluateDual(model_name, global_X, global_y, local_X, local_y):
+    # Model Dependencies
+    dependencies = {
+        'f1_m': f1_m,
+        'precision_m': precision_m,
+        'recall_m': recall_m,
+        'auc_roc': tf.keras.metrics.AUC(),
+        'num_classes': 1,
+        'input_shape': (local_X.shape[1], global_X.shape[1], 1)
+    }
+
+    # Get Model
+    model = load_model(model_name, custom_objects=dependencies)
+
+    score_loss = []
+    score_acc = []
+    score_f1 = []
+    score_prec = []
+    score_rec = []
+    score_auc = []
+    for i in range(50):
+        global_X_shuf, global_y_shuf, local_X_shuf, local_y_shuf = shuffle(
+            global_X, global_y, local_X, local_y)
+
+        # Evaluate Model
+        score = model.evaluate(
+            [global_X_shuf, local_X_shuf], global_y_shuf, verbose=0)
+
+        score_loss.append(score[0])
+        score_acc.append(score[1])
+        score_f1.append(score[2])
+        score_prec.append(score[3])
+        score_rec.append(score[4])
+        score_auc.append(score[5])
+
+    # Print results
+    print("\n------------------ Model : {} ---------------------".format(model_name))
+    print("{}: {:0.2f} ".format(model.metrics_names[0], np.mean(score_loss)))
+    print("%s: %.2f%% " %
+          (model.metrics_names[1], np.mean(score_acc)))
+    print("%s: %.2f%% " %
+          (model.metrics_names[2], np.mean(score_f1)))
+    print("%s: %.2f%% " %
+          (model.metrics_names[3], np.mean(score_prec)))
+    print("%s: %.2f%% " %
+          (model.metrics_names[4], np.mean(score_rec)))
+    print("%s: %.2f%% " %
+          (model.metrics_names[5], np.mean(score_auc)))
+
+    print("---------------------------------------------------")
+
+
 if __name__ == "__main__":
     data_global = np.loadtxt(
         'data/Shallue/separated/global_test.csv', delimiter=',')
@@ -96,4 +148,6 @@ if __name__ == "__main__":
     local_X = np.expand_dims(
         local_X, axis=2)
 
-    evaluate('models/Approved/model_LSTM1.hdf5', local_X, local_Y)
+    #evaluate('models/Approved/model_LSTM1.hdf5', local_X, local_Y)
+    evaluateDual('models/Approved/model_LSTM1.hdf5',
+                 global_X, global_Y, local_X, local_Y)
