b5df981979f74856faf8e02f979c3d530581ddb0
DiogorPinheiro <diogomrpinheiro@gmail.com>
2020-06-10 22:44:44 +0100

    earth++

--- src/utilities.py
+++ src/utilities.py
@@ -3,6 +3,15 @@ import csv
 
 
 def concatenate(gl, lo):
+    '''
+        Concatenates two lists.
+
+        @param gl (np.array(float)): global values
+        @param lo (np.array(float)): local values
+
+        @return (np.array(float)): concatencated values of both views
+
+    '''
     gl = gl.tolist()
     lo = lo.tolist()
     for index, a in enumerate(lo):
@@ -13,6 +22,15 @@ def concatenate(gl, lo):
 
 
 def joinLists(data1, data2):
+    '''
+        Joins two lists.
+
+        @param data1 (np.array(float)): values of the first data array
+        @param data2 (np.array(float)): values of the second data array
+
+        @return (np.array(float)): joined values of both views
+
+    '''
     for index, a in enumerate(data2):
         for b in a:
             data1[index].append(b)
@@ -21,6 +39,13 @@ def joinLists(data1, data2):
 
 
 def writeToFile(name, data):
+    '''
+        Write data to file.
+
+        @param name (String): name of file
+        @param data (np.array(float)): data to be written to file
+
+    '''
     with open(name, "w") as fd:  # Write Header
         writer = csv.writer(fd)
         writer.writerow(data)
